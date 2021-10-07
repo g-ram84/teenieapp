e45fed027e02ed04620fddf7d47afe80a87eba0b
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-10-07 22:35:33 +0000

    updated new url form

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+	let result = "";
+	let characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy1234567890";
+	for (let i = 0; i < 6; i++) {
+		result += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return result;
+}
 
 const urlDatabase = {
 	b2xVn2: "http://www.lighthouselabs.ca",
@@ -26,6 +38,10 @@ app.get("/urls", (req, res) => {
 	res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+	res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
 	const shortURL = req.params.shortURL;
 	const templateVars = {
@@ -34,6 +50,13 @@ app.get("/urls/:shortURL", (req, res) => {
 	};
 	res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+	console.log(req.body);
+	res.send("OK");
+});
+
+console.log(generateRandomString());
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
